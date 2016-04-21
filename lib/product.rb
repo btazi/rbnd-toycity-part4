ae105534c67fe26779867ee16bd94c9939607d71
Badr Tazi <tasibadr@gmail.com>
2016-04-22 00:04:05 +0100

    add product#all & product#create

--- lib/product.rb
+++ lib/product.rb
@@ -4,7 +4,6 @@ class Product < Udacidata
   attr_reader :id, :price, :brand, :name
 
   def initialize(opts={})
-
     # Get last ID from the database if ID exists
     get_last_id
     # Set the ID if it was passed in, otherwise use last existing ID
@@ -16,6 +15,37 @@ class Product < Udacidata
     @name = opts[:name]
     @price = opts[:price]
   end
+
+	def self.create(attributes={})
+		data_path = File.dirname(__FILE__) + "/../data/data.csv"
+		attributes = {brand: attributes[:brand], name: attributes[:name], price: attributes[:price]}
+		product = Product.new(attributes)
+		#check if product already exists
+		unless product.exists?
+			CSV.open(data_path, "a+") do |csv|
+				csv << [product.id, product.brand, product.name, product.price]
+			end
+		end
+		product
+	end
+
+	def self.all
+		data_path = File.dirname(__FILE__) + "/../data/data.csv"
+		products = []
+		CSV.foreach(data_path, headers: true).each do |row|
+				product = Product.new(id: row["id"], brand: row["brand"], name: row["product"], price: row["price"])
+				products << product
+		end
+		products
+	end
+
+	def self.first
+		self.all.first
+	end
+
+	def exists?
+		Product.all.any?{|product|  product.id == id && product.brand == brand && product.name == name && product.price.to_i == price}
+	end
 
   private
 
